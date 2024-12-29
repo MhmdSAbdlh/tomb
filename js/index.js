8dd045518bb613b9b35dbb30d46d8e2bb3f2a7cd
Mohamad Abdallah <hamadakakos@gmail.com>
2024-12-29 20:23:16 -0300

    add new peoples and fixed
    1- Change the intro photo
    2- Add always on top for the progressbar when go down
    3- Add new peoples
    4- Add separator above the lowers buttons
    5- The timer for the random is on by default
    6- Change the open photo to the soon app to dialog
    7- Change some texts
    8- Change th border to the disabled buttons
    9- For the intro, the surat has buttons that disabled when it clicked and disappear when finish
    10- Auto change the text size according to the screen

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 var btn = new Array();
 var progress = document.getElementById("myBar");
-var temp =0;
+var temp =1;
 init();
+
+// Make the progress bar "stick" at the top when scrolling down
+const progressBarWrapper = document.getElementById("progressBarWrapper");
+const initialOffsetTop = progressBarWrapper.offsetTop;
+
+window.onscroll = function() {
+	if (window.scrollY > initialOffsetTop) {
+        progressBarWrapper.classList.add("fixed-progress");
+      } else {
+        progressBarWrapper.classList.remove("fixed-progress");
+      }
+};
 
 function init(){
 	//Define the buttons
@@ -14,7 +26,7 @@ function init(){
 	"إم عبدالله","أبو عبدالله",
 	"الشهيد خالد","حسين عدنان","أمينة عبدالله","زينب سلام","فياض عبدالله","أبو طلال",
 	"المختار","المختارة","نوال يوسف","الحج عدنان","أبو بلال",
-	"الشهيد سلمان"
+	"الشهيد سلمان",'الشهيد حسين رمضان','الشهيد كمال عبدالله','أبو عباس عبدالله'
 ];
 	for(let i=0;i<sowar.length;i++){
 		btn[i] = document.createElement("button");
@@ -26,10 +38,10 @@ function init(){
 			btn[i].className = "disabledBtn";
 			btn[i].disabled = true;
 			window.localStorage.setItem(localStorage.length+1,btn[i].innerHTML);
-			document.getElementById('leftS').innerText = "القبور المتبقية: " +(36-localStorage.length); 
+			document.getElementById('leftS').innerText = "القبور المتبقية: " +(39-localStorage.length); 
 			document.getElementById('doneS').innerText = "القبور المنجزة: "+localStorage.length;
-			document.getElementById('todayS').innerText = "الفاتحة عن روح: " +localStorage.getItem(localStorage.length);
-			progress.style.width = localStorage.length * 100 / 36 + "%";
+			document.getElementById('todayS').innerText = "عن روح " +localStorage.getItem(localStorage.length);
+			progress.style.width = localStorage.length * 100 / 39 + "%";
 			endRead();
 		}
 	}
@@ -80,6 +92,9 @@ function init(){
 	document.getElementById('bodyDiv9').appendChild(btn[34]);
 
 	document.getElementById('bodyDiv10').appendChild(btn[35]);
+	document.getElementById('bodyDiv10').appendChild(btn[36]);
+	document.getElementById('bodyDiv10').appendChild(btn[37]);
+	document.getElementById('bodyDiv10').appendChild(btn[38]);
 
 	//Import from local storage
 	for(let i=0; i<btn.length; i++){
@@ -87,11 +102,11 @@ function init(){
 			if(btn[i].innerHTML == window.localStorage.getItem(k)){
 				btn[i].className = "disabledBtn";
 				btn[i].disabled = true;
-				if(i == 3 || i == 8 || i == 24 || i == 25){
+				if(i == 3 || i == 8 || i == 24 || i == 25 || i == 36){
 					btn[i].style.backgroundColor = 'gold';
 					btn[i].style.color = 'black';
 				}
-				else if(i == 5 || i == 16 || i == 21 || i == 35){
+				else if(i == 5 || i == 16 || i == 21 || i == 35 || i == 37){
 					btn[i].style.backgroundColor = 'black';
 					btn[i].style.color = 'white';
 				}
@@ -99,15 +114,15 @@ function init(){
 		}
 	}
 
-	progress.style.width = localStorage.length * 100 / 36 + "%";
+	progress.style.width = localStorage.length * 100 / 39 + "%";
 	document.getElementById('doneS').innerText = "القبور المنجزة: "+localStorage.length;
-	document.getElementById('leftS').innerText = "القبور المتبقية: "  +(36-localStorage.length);
+	document.getElementById('leftS').innerText = "القبور المتبقية: "  +(39-localStorage.length);
 	endRead();
 	if(localStorage.length==0){
-		document.getElementById('todayS').innerText = "الفاتحة عن روح: غير محدد";
+		document.getElementById('todayS').innerText = "غير محدد";
 	}
 	else{
-		document.getElementById('todayS').innerText = "الفاتحة عن روح: "+localStorage.getItem(localStorage.length);
+		document.getElementById('todayS').innerText = "عن روح "+localStorage.getItem(localStorage.length);
 	}
 }
 
@@ -126,20 +141,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //Select random vaLUE
 function randomS(){
-	let index = Math.floor(Math.random()*36);
+	let index = Math.floor(Math.random()*39);
 	while(btn[index].disabled == true){//CHECK IF THE RANDOM VALUE ALREADY EXIST
-		index = Math.floor(Math.random()*36);
+		index = Math.floor(Math.random()*39);
 	}
 	specialF(index);
-	if(localStorage.length<36){
+	if(localStorage.length<39){
 		btn[index].className = "disabledBtn";
 		btn[index].disabled = true;
 		window.localStorage.setItem(localStorage.length+1,btn[index].innerHTML);
-		document.getElementById('leftS').innerText = "القبور المتبقية: "  +(36-localStorage.length); 
+		document.getElementById('leftS').innerText = "القبور المتبقية: "  +(39-localStorage.length); 
 		document.getElementById('doneS').innerText = "القبور المنجزة: "+localStorage.length;
-		document.getElementById('todayS').innerText = "الفاتحة عن روح: " +localStorage.getItem(localStorage.length);
-		progress.style.width = localStorage.length * 100 / 36 + "%";
-		if((index != 3 && index != 5 && index != 8 && index !=16  && index !=21  && index != 24 && index != 25 && index != 35) && temp ==1 )//special case
+		document.getElementById('todayS').innerText = "عن روح " +localStorage.getItem(localStorage.length);
+		progress.style.width = localStorage.length * 100 / 39 + "%";
+		if((index != 3 && index != 5 && index != 8 && index !=16  && index !=21  && index != 24 && index != 25 && index != 35 && index != 36 && index != 37) && temp ==1 )//special case
 		Swal.fire({
 			position: 'bottom',
 			toast: false,
@@ -167,7 +182,7 @@ function specialF(index){//if the name is special
 			'info'
 		  )
 	}
-	else if(index == 5 || index == 16 || index == 21 || index == 35){// shahid
+	else if(index == 5 || index == 16 || index == 21 || index == 35 || index == 37){// shahid
 		btn[index].style.backgroundColor = 'black';
 		btn[index].style.color = 'white';
 		Swal.fire(
@@ -176,7 +191,7 @@ function specialF(index){//if the name is special
 			'info'
 		  )
 	}
-	else if(index == 24){//special+shhaid
+	else if(index == 24  || index == 36){//special+shhaid
 		btn[index].style.backgroundColor = 'gold';
 		btn[index].style.color = 'black';
 		Swal.fire(
@@ -196,17 +211,57 @@ function clearS(){
 	}
 	localStorage.clear();
 	document.getElementById('doneS').innerText = "القبور المنجزة: "+localStorage.length;
-	document.getElementById('leftS').innerText = "القبور المتبقية: "+(36-localStorage.length); 
-	document.getElementById('todayS').innerText = "الفاتحة عن روح: غير محدد";
-	progress.style.width = localStorage.length * 100 / 36 + "%";
+	document.getElementById('leftS').innerText = "القبور المتبقية: "+(39-localStorage.length); 
+	document.getElementById('todayS').innerText = "غير محدد";
+	progress.style.width = localStorage.length * 100 / 39 + "%";
 }
 
 function introF(){
-	Swal.fire(
-		'مستحبات أخرى',
-		'قراءة سورة الفاتحة مرة واحدة والتوحيد 11 مرة عن أروح جميع أمواتنا.',
-		'success'
-	  )
+	let count = 11;
+	Swal.fire({
+        title: 'مستحبات أخرى',
+        icon: 'info',
+        html: `
+          <p>قراءة سورة الفاتحة مرة واحدة والتوحيد 11 مرة عن أروح جميع أمواتنا.</p>
+          <button id="button1" class="swal-custom-btn">الفاتحة</button>
+          <button id="button2" class="swal-custom-btn">الاخلاص 11 مرة</button>
+        `,
+        buttonsStyling: true, // Use default or custom styling
+		showConfirmButton: false, // Hide default confirm button
+      })
+
+	  const button1 = document.getElementById('button1');
+      const button2 = document.getElementById('button2');
+
+      // Add event listeners to custom buttons
+      button1.addEventListener('click', function () {
+        this.disabled = true; // Disable the button
+			this.textContent = 'تم'
+        checkAndClose(); // Check if both buttons are disabled
+      });
+
+	  button2.addEventListener('click', function () {
+       count--;
+	    if(count ==0){
+			this.disabled = true; // Disable the button
+			this.textContent = 'تم'
+		}
+		else
+        this.textContent = 'الاخلاص '+count + ' مرة'; // Update text
+        checkAndClose(); // Check if both buttons are disabled
+      });
+
+	  function checkAndClose() {
+        // Check if both buttons are disabled
+        if (button1.disabled && button2.disabled) {
+			Swal.fire({
+				icon: "success",
+				title: "تم بنجاح",
+				showConfirmButton: false,
+				timer: 1000
+			  });
+        }
+      }
 }
 
 function goBack(){
@@ -222,20 +277,20 @@ function goBack(){
 		}
 		localStorage.removeItem(localStorage.length);
 		document.getElementById('doneS').innerText = "القبور المنجزة: "+localStorage.length;
-		document.getElementById('leftS').innerText = "القبور المتبقية: "+(36-localStorage.length); 
-		document.getElementById('todayS').innerText = "الفاتحة عن روح: "+localStorage.getItem(localStorage.length);
-		progress.style.width = localStorage.length * 100 / 36 + "%";
+		document.getElementById('leftS').innerText = "القبور المتبقية: "+(39-localStorage.length); 
+		document.getElementById('todayS').innerText = "عن روح "+localStorage.getItem(localStorage.length);
+		progress.style.width = localStorage.length * 100 / 39 + "%";
 		if(localStorage.length==0){
-			document.getElementById('todayS').innerText = "الفاتحة عن روح: غير محدد";
+			document.getElementById('todayS').innerText = "غير محدد";
 		}
 		else{
-			document.getElementById('todayS').innerText = "الفاتحة عن روح: "+localStorage.getItem(localStorage.length);
+			document.getElementById('todayS').innerText = "عن روح "+localStorage.getItem(localStorage.length);
 		}
 	}
 }
 
 function getValue(){
-	switch (localStorage.getItem(36)) {
+	switch (localStorage.getItem(39)) {
 		case btn[3].innerHTML:
 			return 3;
 		case btn[5].innerHTML:
@@ -252,6 +307,10 @@ function getValue(){
 					return 25;
 				case btn[35].innerHTML:
 					return 35;
+					case btn[36].innerHTML:
+						return 36;
+						case btn[37].innerHTML:
+							return 37;
 				
 		default:
 		  console.log(`Sorry, we are out of ${expr}.`);
@@ -260,11 +319,12 @@ function getValue(){
 }
 
 function endRead(){
-	if(localStorage.length == 36){ //the last one is special	
-		if(localStorage.getItem(36) == btn[24].innerHTML || localStorage.getItem(36) == btn[5].innerHTML || localStorage.getItem(36) == btn[16].innerHTML 
-		|| localStorage.getItem(36) == btn[21].innerHTML || localStorage.getItem(36) == btn[35].innerHTML)//special+shhaid
+	if(localStorage.length == 39){ //the last one is special	
+		if(localStorage.getItem(39) == btn[24].innerHTML || localStorage.getItem(39) == btn[5].innerHTML || localStorage.getItem(36) == btn[16].innerHTML 
+		|| localStorage.getItem(39) == btn[21].innerHTML || localStorage.getItem(39) == btn[35].innerHTML  || localStorage.getItem(39) == btn[36].innerHTML
+		|| localStorage.getItem(39) == btn[37].innerHTML)//special+shhaid
 			Swal.fire(
-				'عن روح '+localStorage.getItem(36),
+				'عن روح '+localStorage.getItem(39),
 				'قراءة سورة التوحيد 3 مرات و القدر مرة ,يا شهيداً عند الله, إشفع لنا عند الله.',
 				'info'
 			  ).then(function() {
@@ -272,7 +332,7 @@ function endRead(){
 				  })
 		else
 		Swal.fire(
-			'عن روح '+localStorage.getItem(36),
+			'عن روح '+localStorage.getItem(39),
 			'قراءة سورة التوحيد 3 مرات و القدر مرة .',
 			'info'
 		  ).then(function() {
@@ -280,66 +340,97 @@ function endRead(){
 			  })
 }
 else{
-	if(localStorage.length == 35 && //before the last one is not special and the last is
-		localStorage.getItem(35) != btn[3].innerHTML && 
-		localStorage.getItem(35) != btn[8].innerHTML && 
-		localStorage.getItem(35) != btn[25].innerHTML &&
-		localStorage.getItem(35) != btn[24].innerHTM && 
+	if(localStorage.length == 38 && //before the last one is not special and the last is
+		localStorage.getItem(38) != btn[3].innerHTML && 
+		localStorage.getItem(38) != btn[8].innerHTML && 
+		localStorage.getItem(38) != btn[25].innerHTML &&
+		localStorage.getItem(38) != btn[24].innerHTM && 
+		localStorage.getItem(38) != btn[36].innerHTM && 
 		(!btn[3].disabled||
 		!btn[8].disabled ||
 		!btn[24].disabled ||
-		!btn[25].disabled))
+		!btn[25].disabled ||
+		!btn[36].disabled))
 		Swal.fire(
-			'عن روح '+localStorage.getItem(35),
+			'عن روح '+localStorage.getItem(38),
 			'قراءة سورة التوحيد 3 مرات و القدر مرة .',
 			'info'
 		  )
 		  else
-		  if(localStorage.length == 34 && //two before the last is not special and the last two is
-			localStorage.getItem(34) != btn[3].innerHTML && 
-			localStorage.getItem(34) != btn[8].innerHTML && 
-			localStorage.getItem(34) != btn[25].innerHTML &&
-			localStorage.getItem(34) != btn[24].innerHTM && 
+		  if(localStorage.length == 37 && //two before the last is not special and the last two is
+			localStorage.getItem(37) != btn[3].innerHTML && 
+			localStorage.getItem(37) != btn[8].innerHTML && 
+			localStorage.getItem(37) != btn[25].innerHTML &&
+			localStorage.getItem(37) != btn[24].innerHTM && 
+			localStorage.getItem(37) != btn[36].innerHTM && 
 			((!btn[3].disabled && !btn[8].disabled)||
 			(!btn[3].disabled && !btn[24].disabled)||
 			(!btn[3].disabled && !btn[25].disabled)||
+			(!btn[3].disabled && !btn[36].disabled)||
 			(!btn[8].disabled && !btn[24].disabled)||
 			(!btn[8].disabled && !btn[25].disabled)||
-			(!btn[24].disabled && !btn[25].disabled)
+			(!btn[8].disabled && !btn[36].disabled)||
+			(!btn[24].disabled && !btn[25].disabled)||
+			(!btn[24].disabled && !btn[36].disabled)||
+			(!btn[36].disabled && !btn[25].disabled)
 			))
 			Swal.fire(
-				'عن روح '+localStorage.getItem(34),
+				'عن روح '+localStorage.getItem(37),
 				'قراءة سورة التوحيد 3 مرات و القدر مرة .',
 				'info'
 			  )
 			  else 
-			  if(localStorage.length == 33 && //3 before the last is not special and the last 3 is
-				localStorage.getItem(33) != btn[3].innerHTML &&
-				localStorage.getItem(33) != btn[8].innerHTML &&
-				localStorage.getItem(33) != btn[25].innerHTML &&
-				localStorage.getItem(33) != btn[24].innerHTM &&
+			  if(localStorage.length == 36 && //3 before the last is not special and the last 3 is
+				localStorage.getItem(36) != btn[3].innerHTML &&
+				localStorage.getItem(36) != btn[8].innerHTML &&
+				localStorage.getItem(36) != btn[25].innerHTML &&
+				localStorage.getItem(36) != btn[24].innerHTM &&
+				localStorage.getItem(36) != btn[36].innerHTM &&
 				((!btn[3].disabled && !btn[8].disabled && !btn[24].disabled)||
 				(!btn[3].disabled && !btn[8].disabled && !btn[25].disabled)||
+				(!btn[3].disabled && !btn[8].disabled && !btn[36].disabled)||
 				(!btn[3].disabled && !btn[24].disabled && !btn[25].disabled)||
-				(!btn[24].disabled && !btn[25].disabled && !btn[8].disabled)
+				(!btn[3].disabled && !btn[24].disabled && !btn[36].disabled)||
+				(!btn[3].disabled && !btn[25].disabled && !btn[36].disabled)||
+				(!btn[8].disabled && !btn[24].disabled && !btn[25].disabled)||
+				(!btn[8].disabled && !btn[24].disabled && !btn[36].disabled)||
+				(!btn[8].disabled && !btn[25].disabled && !btn[36].disabled)||
+				(!btn[24].disabled && !btn[25].disabled && !btn[36].disabled)
 				))
 				Swal.fire(
-					'عن روح '+localStorage.getItem(33),
+					'عن روح '+localStorage.getItem(36),
 					'قراءة سورة التوحيد 3 مرات و القدر مرة .',
 					'info'
 				  )
-				  else
-				  if(localStorage.length == 32 && //before the last 4 is not special and the last 4 is
-					localStorage.getItem(32) != btn[3].innerHTML &&
-					localStorage.getItem(32) != btn[8].innerHTML &&
-					localStorage.getItem(32) != btn[25].innerHTML &&
-					localStorage.getItem(32) != btn[24].innerHTM &&
-					!btn[3].disabled && !btn[8].disabled && !btn[24].disabled && !btn[25].disabled )
+				  else  if(localStorage.length == 35 && //before the last 4 is not special and the last 4 is
+					localStorage.getItem(35) != btn[3].innerHTML &&
+					localStorage.getItem(35) != btn[8].innerHTML &&
+					localStorage.getItem(35) != btn[25].innerHTML &&
+					localStorage.getItem(35) != btn[24].innerHTM &&
+					localStorage.getItem(35) != btn[36].innerHTM &&
+					((!btn[3].disabled && !btn[8].disabled && !btn[24].disabled && !btn[25].disabled) ||
+					(!btn[3].disabled && !btn[8].disabled && !btn[24].disabled && !btn[36].disabled) ||
+				(!btn[3].disabled && !btn[8].disabled && !btn[25].disabled && !btn[36].disabled)||
+			(!btn[3].disabled && !btn[24].disabled && !btn[25].disabled && !btn[36].disabled)||
+			(!btn[8].disabled && !btn[24].disabled && !btn[25].disabled && !btn[36].disabled) 
+				 ))
 					Swal.fire(
-						'عن روح '+localStorage.getItem(32),
+						'عن روح '+localStorage.getItem(35),
 						'قراءة سورة التوحيد 3 مرات و القدر مرة .',
 						'info'
 					  )
+					  else  if(localStorage.length == 34 && //before the last 5 is not special and the last 4 is
+						localStorage.getItem(34) != btn[3].innerHTML &&
+						localStorage.getItem(34) != btn[8].innerHTML &&
+						localStorage.getItem(34) != btn[25].innerHTML &&
+						localStorage.getItem(34) != btn[24].innerHTM &&
+						localStorage.getItem(34) != btn[36].innerHTM &&
+						(!btn[3].disabled && !btn[8].disabled && !btn[24].disabled && !btn[25].disabled && !btn[36].disabled))
+						Swal.fire(
+							'عن روح '+localStorage.getItem(34),
+							'قراءة سورة التوحيد 3 مرات و القدر مرة .',
+							'info'
+						  )
 	}
 }
 
@@ -371,4 +462,17 @@ function prayFinal(){
 				clearS();
 			  }
 		  });
+	}
+
+function soonF(){
+	Swal.fire({
+		title: "قريباً",
+		text: "جاري العمل وسيتم اضافته قريباً",
+		imageUrl: "https://raw.githubusercontent.com/MhmdSAbdlh/tomb/refs/heads/main/img/soon.png",
+		timer: 3000,
+		timerProgressBar: true,
+  		imageWidth: 400,
+		imageHeight: 300,
+		imageAlt: "Custom image"
+	  });
 	}
